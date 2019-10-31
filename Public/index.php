9903a604b8530ee58880744e93716d818ff95fba
Jakob Clausen <jakob.fridmar@gmail.com>
2019-10-31 10:14:49 +0100

    Changed some comments

--- Public/index.php
+++ Public/index.php
@@ -12,27 +12,29 @@ require_once __DIR__.'/header.php';
 
                     <div class='articleItem'>
 
-                        <h1><?php echo $articles['title']?></h1>
+                        <h1><?php echo $articles['title']?></h1>  <!-- Title -->
 
-                        <img class="heartLogo hover" src="https://img.icons8.com/ios-glyphs/30/000000/filled-like.png">
+                        <img class="heartLogo hover" src="https://img.icons8.com/ios-glyphs/30/000000/filled-like.png">  <!-- Like heart -->
 
-                        <p class="likes"><?php echo $articles['likes'] ?></p>
+                        <p class="likes"><?php echo $articles['likes'] ?></p>  <!-- Like counter -->
 
-                        <img class="articleImage" src="<?php echo $articles['img']?>" alt="<?php echo $articles['imgAlt']?>">
+                        <img class="articleImage lozad" src="<?php echo $articles['img']?>" alt="<?php echo $articles['imgAlt']?>"> <!-- image (class:"lozad" is for lazy loading.) -->
 
-                        <p class="imageUnderText"><?php echo $articles['imageUnderText'] ?></p>
+                        <p class="imageUnderText"><?php echo $articles['imageUnderText'] ?></p>  <!-- Image description -->
 
-                        <p><?php echo getArticleContent($articles['content']); ?></p>
+                        <p><?php echo getArticleContent($articles['content']); ?></p> <!-- Article content -->
 
-                        <p class="authorName">By: <?php echo getAuthorName($articles['authorId']) ?></p>
+                        <p class="authorName">By: <?php echo getAuthorName($articles['authorId']) ?></p> <!-- Article author -->
 
-                        <p class="date"><?php echo getArticleDate($articles['publishDate']); ?></p>
+                        <p class="date"><?php echo getArticleDate($articles['publishDate']); ?></p> <!-- Publish date -->
 
                     </div>
 
                 <?php endforeach; ?>
 
             </div>
-        </div>
+				</div>
+			<script src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script> <!-- Lazy loading images -->
+
     </body>
 </html>
